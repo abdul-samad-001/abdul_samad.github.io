3d7d9e1d1a732c1956f9f84dda7070eb2558ce44
abdul-samad-001 <abdulsamad9040474@gmail.com>
2026-05-02 21:24:38 +0530

    fix: updated vite base for github pages

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/abdulsamad.github.io/',
+  base: '/',
   server: {
     host: true,
     allowedHosts: true
